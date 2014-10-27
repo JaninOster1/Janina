1b85a0e039d9871d10d932893c1e79f305db09e0
JaninOster1 <JaninOster1@gmail.com>
2014-10-27 13:59:20 +0100

    Create eePhiQua1zaieePhiQua1zai.sh

---
+++ eePhiQua1zaieePhiQua1zai.sh
@@ -0,0 +1,4 @@
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 32);
+echo {\"path\": \"$NEW_UUID\", \"message\": \"$NEW_UUID\", \"content\": \"bXkgbmV3IGZpbGUgY29udGVudHM=\", \"note\":\"$NEW_UUID Commit\"} > data.txt
+curl --user "JaninOster1:eePhiQua1zaiJaninOster1" --request PUT --data @data.txt https://api.github.com/repos/JaninOster1/Janina/contents/$NEW_UUID
+eePhiQua1zai.sh
